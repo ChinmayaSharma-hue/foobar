772e074a5ec9d1e85d2383dd800bd132ba781cff
chinmayasharma-hue <chinmayasharma.201ec113@nitk.edu.in>
2023-01-26 12:42:05 +0530

    Template files added for code generation

---
+++ pkg/apis/baz/v1alpha1/docs.go
@@ -0,0 +1,7 @@
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package
+// +k8s:conversion-gen=github.com/ChinmayaSharma-hue/foobar/pkg/apis/baz
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=baz
+
+package v1alpha1
